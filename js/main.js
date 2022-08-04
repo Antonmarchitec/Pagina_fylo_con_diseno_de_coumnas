8e36faaa32d5a6b3d4a702247ab0361e7c016bb5
mi nombre <desarrollowebaps@gmail.com>
2022-08-03 23:58:11 -0400

    Actualizando js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
 
-
-
 document.addEventListener("click", (e) =>{
     const inputAlert = document.querySelector("#container_sectionOne_form_p")
     const correo = document.querySelector("#inputText").value
-    let expresionRegular = /\w+@\w+\.+[a-z]/;  //para correo electronico
+    
+    let imputCorreo = document.querySelector("#inputTree").value
+    let threeLabel = document.querySelector("#threeLabel")
+
 
     if(e.target.id === "boton"){
-        if(expresionRegular.test(correo)){
-            inputAlert.classList.add("container_sectionOne_form_p_JS")
-            inputAlert.textContent = "El correo fue enviado con exito"
-
-        }else{
-            inputAlert.classList.add("container_sectionOne_form_p_JS")
-            inputAlert.textContent = "El correo no es valido"
-        }
+        validarCorreo(inputAlert, correo)  
+    }else if(e.target.id === "threeButon"){
+        validarCorreo(threeLabel, imputCorreo)
     }
+    
 })
+
+
+//FUNCION VALIDAR CORREO
+const validarCorreo = (labell, correo) =>{
+    let expresionRegular = /\w+@\w+\.+[a-z]/;  //para correo electronico
+
+    if(expresionRegular.test(correo)){
+        labell.classList.add("activar_label_JS")
+        labell.textContent = "El correo fue enviado con exito"
+        labell.style.color = "grey"
+        labell.style.transition = "1s all"
+
+
+    }else{
+        labell.classList.add("activar_label_JS")
+        labell.textContent = "El correo no es valido"
+        labell.style.color = "red"
+        labell.style.transition = "1s all"
+
+    }
+}
+
